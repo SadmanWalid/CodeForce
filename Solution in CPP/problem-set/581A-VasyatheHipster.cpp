eefbc03d15537afb8b35fa04af896e102e63551a
Sadman Walid <40543547+SadmanWalid@users.noreply.github.com>
2021-12-22 18:07:52 +0600

    Added a new solution.

---
+++ Solution in CPP/problem-set/581A-VasyatheHipster.cpp
@@ -0,0 +1,19 @@
+
+#include <bits/stdc++.h>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+
+	if(a<b)
+	 cout<<a<<" "<<(b-a)/2;
+	else
+	cout<<b<<" "<<(a-b)/2;
+
+
+	return 0;
+}
